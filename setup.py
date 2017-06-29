f7f0a2432d789b04f8f87462969deeac79976226
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-06-29 07:45:32 -0400

    Structure for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='niklib',
+  packages=['niklib'],
+  version='1.0',
+  description='Nifty tools and containers',
+  author='Nik Vanderhoof',
+  author_email='pypi@vanderhoof.pw',
+  url='https://github.com/nvander1/niklib',
+  download_url='https://github.com/nvander1/niklib/archive/1.0.tar.gz',
+  keywords=['container', 'namedtuple'],
+  classifiers=[]
+)
