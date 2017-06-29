8c73f1845a1807b506cac1797e2f1247cde5a164
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-06-29 08:10:14 -0400

    Stop using borked local pylintrc

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 
 from setuptools import setup
 setup(
-  name='nik',
-  packages=['nik'],
-  version='1.0',
-  description='Nifty tools and containers',
-  author='Nik Vanderhoof',
-  author_email='pypi@vanderhoof.pw',
-  url='https://github.com/nvander1/nik',
-  download_url='https://github.com/nvander1/nik/archive/1.0.tar.gz',
-  keywords=['container', 'namedtuple'],
-  classifiers=[]
+    name='nik',
+    packages=['nik'],
+    version='1.0',
+    description='Nifty tools and containers',
+    author='Nik Vanderhoof',
+    author_email='pypi@vanderhoof.pw',
+    url='https://github.com/nvander1/nik',
+    download_url='https://github.com/nvander1/nik/archive/1.0.tar.gz',
+    keywords=['container', 'namedtuple'],
+    classifiers=[]
 )
