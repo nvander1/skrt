b25a58d45b0db037e9b02b761decf173e12fcad4
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-07-03 18:05:23 -0400

    Follow official python packaging

--- setup.py
+++ setup.py
@@ -1,15 +1,34 @@
-"""Setup for PyPi"""
+"""Setup module for nik.
 
+See:
+https://github.com/nvander1/nik
+"""
+
+from codecs import open
+from os import path
 from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='nik',
-    packages=['nik'],
-    version='1.0',
+    version='1.0.0',
     description='Nifty tools and containers',
+    long_description=long_description,
+    url='https://github.com/nvander1/nik',
     author='Nik Vanderhoof',
     author_email='pypi@vanderhoof.pw',
-    url='https://github.com/nvander1/nik',
-    download_url='https://github.com/nvander1/nik/archive/1.0.tar.gz',
-    keywords=['container', 'namedtuple'],
-    classifiers=[]
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6'
+    ],
+    keywords='development tools containers',
+    packages=['nik'],
+    python_requires='~=3.6'
 )
