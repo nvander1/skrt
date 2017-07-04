01775ed317aeb20a278b57136784905e774c5356
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-07-04 02:12:38 -0400

    Add tests for defaultnamedtuple

---
+++ tests/test_defaultnamedtuple.py
@@ -0,0 +1,36 @@
+from nose.tools import raises
+
+from skrt.containers import defaultnamedtuple
+
+
+def setup_module():
+    global Name
+    Name = defaultnamedtuple('Name', 'first', 'last', middle='', prefix='Mr.')
+
+
+def test_positional_arg_assignment():
+    john_doe = Name('John', 'Doe')
+    assert john_doe.first == 'John'
+    assert john_doe.last == 'Doe'
+
+
+@raises(TypeError)
+def test_missing_positional_args():
+    john = Name('John')
+
+
+def test_kwarg_default():
+    mr_john_doe = Name('John', 'Doe')
+    assert mr_john_doe.middle == ''
+    assert mr_john_doe.prefix == 'Mr.'
+
+
+def test_kwarg_assign():
+    dr_john_doe = Name('John', 'Doe', prefix='Dr.')
+    assert dr_john_doe.prefix == 'Dr.'
+
+
+def test_repr():
+    john_doe = Name('John', 'Doe', middle='M')
+    assert repr(john_doe) == ("Name(first='John', last='Doe', "
+                              "middle='M', prefix='Mr.')")
