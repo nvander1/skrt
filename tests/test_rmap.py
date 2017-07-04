823956ee77caef24566a4ebe29992cd0922c6ebf
Jack Fischer <jack.fischer11@gmail.com>
2017-07-04 10:38:39 -0400

    swap arguments in API to match stdlib map

--- tests/test_rmap.py
+++ tests/test_rmap.py
@@ -7,31 +7,30 @@ def square(x):
 
 def test_list():
     list_ = [1, 2, 3, 4, 5]
-    assert rmap(list_, square, int) == [1, 4, 9, 16, 25]
+    assert rmap(square, list_, int) == [1, 4, 9, 16, 25]
 
 
 def test_tuple():
     tuple_ = (1, 2, 3, 4, 5)
-    assert rmap(tuple_, square, int) == (1, 4, 9, 16, 25)
+    assert rmap(square, tuple_, int) == (1, 4, 9, 16, 25)
 
 
 def test_set():
     set_ = {1, 2, 3, 4, 5}
-    assert rmap(set_, square, int) == {1, 4, 9, 16, 25}
+    assert rmap(square, set_, int) == {1, 4, 9, 16, 25}
 
 
 def test_dict():
     dict_ = {'a': 1, 'b': 2, 'c': 3}
-    assert rmap(dict_, square, int) == {'a': 1, 'b': 4, 'c': 9}
+    assert rmap(square, dict_, int) == {'a': 1, 'b': 4, 'c': 9}
 
 
 def test_complex_nested():
     obj = ['1', 2, ({3: 4}, {5, '6'})]
-    assert rmap(obj, square, int) == ['1', 4, ({3: 16}, {'6', 25})]
+    assert rmap(square, obj, int) == ['1', 4, ({3: 16}, {'6', 25})]
 
 
 def test_unusual_mapping():
     from collections import Counter
     c = Counter([1,2,2])
-    assert rmap(c, square, int) == Counter([1,2,2,2,2])
-
+    assert rmap(square, c, int) == Counter([1,2,2,2,2])
